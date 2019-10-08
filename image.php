84c508f7333964d75d77dfd57131dd4817ab83eb
michaelpeeters <mich.peeters@gmail.com>
2019-10-08 16:58:58 +0200

    Update image.php

--- image.php
+++ image.php
@@ -5,6 +5,7 @@ $white = imagecolorallocate($im, 255, 255, 255);
 // Line reserved to trigger the webhook in the lab. 
 $deployment = getenv("COLOR");
 
+$color = imagecolorallocate($im, 255,0,0);
 
 // Draw a filled rectangle
 imagefilledrectangle($im, 0, 0, 199, 199, $color);
